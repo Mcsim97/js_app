6ae757b80da9692f95bfeea4b812b223cc27704e
Mcsim97 <94051350+Mcsim97@users.noreply.github.com>
2022-07-16 18:09:53 +0300

    answer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+const presonalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?'),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов?'),
+      d = prompt('На сколько оцените его?');
+presonalMovieDB.movies[a] = b;
+presonalMovieDB.movies[c] = d;
+
+console.log(presonalMovieDB);
+
 
 
 
